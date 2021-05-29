1597ea9702c23d41d53d4fb690db40dc71640fab
WasBart <WasBart@users.noreply.github.com>
2021-05-29 17:53:58 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -10,16 +10,16 @@ class WorkoutSkill(ChatterboxSkill):
     @intent_handler('workout.intent')
     def handle_workout_intent(self, message):
         self.speak('Tell me about your workout')
-        pushups = self.get_response('How many pushups did you do?')
-        situps = self.get_response('How many situps did you do?')
+        self.pushups = self.get_response('How many pushups did you do?')
+        self.situps = self.get_response('How many situps did you do?')
         self.speak('Thanks for recording your workout')
 
     @intent_handler('recap.intent')
     def handle_recap_intent(self, message):
         self.speak('This is how many pushups you did')
-        self.speak(pushups)
+        self.speak(self.pushups)
         self.speak('This is how many situps you did')
-        self.speak(situps)
+        self.speak(self.situps)
 
     def stop(self):
         pass
