e84f2f16d8d589e3fa1bb6b6abc90565c724a21e
WasBart <wassily.bartuska@gmail.com>
2021-06-21 00:20:20 +0200

    Changes workout recording to accept more exercises.

--- __init__.py
+++ __init__.py
@@ -6,13 +6,24 @@ class WorkoutSkill(ChatterboxSkill):
         super().__init__()
         self.pushups = ""
         self.situps = ""
+        self.exercises = {}
 
+    #@intent_handler('workout.intent')
+    #def handle_workout_intent(self, message):
+    #    self.speak('Tell me about your workout')
+    #    self.pushups = self.get_response('How many pushups did you do?')
+    #    self.situps = self.get_response('How many situps did you do?')
+    #    self.speak('Thanks for recording your workout')
     @intent_handler('workout.intent')
     def handle_workout_intent(self, message):
+        more_exercises = True
         self.speak('Tell me about your workout')
-        self.pushups = self.get_response('How many pushups did you do?')
-        self.situps = self.get_response('How many situps did you do?')
-        self.speak('Thanks for recording your workout')
+        while more_exercises:
+            exercise = self.get_response('What was the exercise?')
+            reps = self.get_response('How many repititions did you do?')
+            done = self.get_response('Is that all?')
+            if done == 'Yes':
+                more_exercises = False
 
     @intent_handler('recap.intent')
     def handle_recap_intent(self, message):
